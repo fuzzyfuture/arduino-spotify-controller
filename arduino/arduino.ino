ad1d7d2a1c9f1e271247f06a3600575fd4aa6360
Nathan Chay <nathangchay@gmail.com>
2020-12-03 02:49:05 -0600

    commented code :100:

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,6 +2,7 @@
 #include <hd44780.h>
 #include <hd44780ioClass/hd44780_I2Cexp.h>
 
+//  2 x 16 display
 hd44780_I2Cexp lcd;
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
@@ -12,6 +13,7 @@ const String BUTTON_NAMES[6] = {"Shuffle:", "Loop:", "Prev:", "Play:", "Next:", 
 
 //  pin of potentiometer
 const int VOLUME_PIN = A0;
+//  volume update will only be sent if new volume is higher or lower than the current volume by this amount
 const int VOLUME_THRES = 5;
 
 // stores last state of button (initialized to 0)
@@ -23,13 +25,17 @@ int volume_last_state = 0;
 //  stores current state of button for loop
 int button_current_state = 0;
 
+//  stores name of currently playing song
 String song_current_name = "";
 
+//  raw input from potentiometer and converted volume
 int volume_raw = 0;
 int volume = 0;
 
+//  stores name of current button for loop
 String button_name = "";
 
+//  stores last input from serial
 String serial_input = "";
 bool serial_input_complete = false;
 
@@ -44,8 +50,10 @@ void setup()
   //  initialize serial output
   Serial.begin(9600);
 
+  //  start lcd
   lcd.begin(LCD_COLS, LCD_ROWS);
-  
+
+  //  reserve 
   serial_input.reserve(200);
 }
 
@@ -57,59 +65,79 @@ void loop()
     //  getting current state of button
     button_current_state = digitalRead(BUTTON_PINS[i]);
 
+    //  button pressed
     if (button_last_state[i] == HIGH && button_current_state == LOW)
     {
       button_name = BUTTON_NAMES[i];
-      
+
+      // send name to serial and update current button state
       Serial.println(button_name);
       button_last_state[i] = LOW;
 
+      //  print button name to the lower portion of the lcd
       lcd.clear();
  
       lcd.setCursor(0, 1);
       lcd.print(button_name.substring(0, button_name.indexOf(":")));
 
+      //  only want to show temporarily
       delay(300);
 
       lcd.clear();
     }
+
+    //  button released
     else if (button_last_state[i] == LOW && button_current_state == HIGH)
     {
       button_last_state[i] = HIGH;
     }
   }
 
+  //  reading volume from potentiometer
   volume_raw = analogRead(VOLUME_PIN);
+  //  spotify handles volume via percentage (0%-100%) so we convert the raw reading
   volume = map(volume_raw, 0, 1023, 0, 100);
 
+  //  threshold prevents instability (e.g. volume rapidly going between 72 and 73) due to inaccuracy of potentiometer
   if (volume >= volume_last_state + VOLUME_THRES || volume <= volume_last_state - VOLUME_THRES)
   {
+    //  send the new volume
     Serial.print("Volume:");
     Serial.println(volume);
-    
+
+    //  update state
     volume_last_state = volume;
 
+    //  print to lower portion of lcd
     lcd.clear();
 
     lcd.setCursor(0, 1);
     lcd.print("Volume: ");
     lcd.print(volume);
 
+    //  only want to show temporarily
     delay(300);
 
     lcd.clear();
   }
 
+  //  checking if new song name recieved via serial is different
+  //  (if not, it is unneccessary to update the lcd)
   if (serial_input_complete && song_current_name != serial_input)
   {
+    //  updating current song
     song_current_name = serial_input;
 
+    //  resetting serial input
     serial_input = "";
     serial_input_complete = false;
 
+    //  clear lcd
     lcd.clear();
   }
 
+  //  print current song name to the top line of the lcd
+  //  and the current artist to the bottom line
   lcd.setCursor(0, 0);
   lcd.print(song_current_name.substring(song_current_name.indexOf("-") + 2));
 
@@ -117,18 +145,24 @@ void loop()
   lcd.print(song_current_name.substring(0, song_current_name.indexOf("-") - 1));
 }
 
+//  this function is automatically triggered every loop()
+//  when there is serial data to be read
+//  https://www.arduino.cc/en/Tutorial/BuiltInExamples/SerialEvent
 void serialEvent()
 {
   while (Serial.available())
   {
+    //  get next character
     char in = (char)Serial.read();
 
+    //  if next character is a new line, the string is finished
     if (in == '\n')
     {
       serial_input_complete = true;
       break;
     }
 
+    //  add next character to string
     serial_input += in;
   }
 }
--- src/InputListener.java
+++ src/InputListener.java
@@ -1,16 +1,11 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -20,33 +15,46 @@ import com.fazecast.jSerialComm.*;
 
 public final class InputListener implements SerialPortMessageListener
 {
+    //  authorization to access spotify api - see readme for more details
     static String access_token = "BQD6zg59xkjuH6kZDryfQ7tRJ-SI-exXR6t8HtbE4445kHC4DaO8LAJxJ6K3Uw5fjXDaVe4YX_oWJxs6iy2X5w18g9EqM52t5gtNyB_Fub08Ph-PwLzaVOD4v06Y1NI-WJ_scaFLt6fpjqGxmgLy5OUbF2USiImcBbdYMppkSp-COvkGPiDvNLjT4TJie6g4AvjfrFDxRoMQx3Tji3S9SSLaH7hLSBcWg5t5pr5cWPvDxI4vDAX36lB7mtEeFTxqpXQlpv_QtQYTyrOIO_79";
+    //  serial port to connect to
     static SerialPort port;
+    //  allows us to send/recieve http requests
     static CloseableHttpClient http_client = HttpClients.createDefault();
 
+    //  handles the response from http requests
     static ResponseHandler<String> response_handler = response->
     {
+        //  response consists of three parts:
+        //  status code (e.g. 404)
         int status = response.getStatusLine().getStatusCode();
+        //  reason phrase (e.g. file not found)
         String msg = response.getStatusLine().getReasonPhrase();
+        //  response data (e.g. some json data)
         String data = "";
 
+        //  only want to attempt to get data if our request returns it
         HttpEntity entity = response.getEntity();
         if (entity != null)
         {
             data = EntityUtils.toString(entity);
         }
 
+        //  complete response
         return "Status: " + status + "\n" + "Response: " + msg + "\n" + "Data: " + data;
     };
 
+    //  submits an http get request
     public static String getRequest(String uri, CloseableHttpClient http_client, ResponseHandler<String> response_handler)
     {
+        //  setting up the request and formatting for spotify's api
         HttpGet req =  new HttpGet(uri);
 
         req.setHeader("Accept", "application/json");
         req.setHeader("Content-Type", "application/json");
         req.setHeader("Authorization", "Bearer " + access_token);
 
+        //  send the request
         try
         {
             return http_client.execute(req, response_handler);
@@ -59,14 +67,17 @@ public final class InputListener implements SerialPortMessageListener
         }
     }
 
+    //  submits an http put request
     public String putRequest(String uri, CloseableHttpClient http_client, ResponseHandler<String> response_handler)
     {
+        //  setting up the request and formatting for spotify's api
         HttpPut req =  new HttpPut(uri);
 
         req.setHeader("Accept", "application/json");
         req.setHeader("Content-Type", "application/json");
         req.setHeader("Authorization", "Bearer " + access_token);
 
+        //  send the request
         try
         {
             return http_client.execute(req, response_handler);
@@ -80,14 +91,17 @@ public final class InputListener implements SerialPortMessageListener
         }
     }
 
+    //  submits an http post request
     public String postRequest(String uri, CloseableHttpClient http_client, ResponseHandler<String> response_handler)
     {
+        //  setting up the request and formatting for spotify's api
         HttpPost req =  new HttpPost(uri);
 
         req.setHeader("Accept", "application/json");
         req.setHeader("Content-Type", "application/json");
         req.setHeader("Authorization", "Bearer " + access_token);
 
+        //  send the request
         try
         {
             return http_client.execute(req, response_handler);
@@ -106,6 +120,7 @@ public final class InputListener implements SerialPortMessageListener
         return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
     }
 
+    //  data read from serial should be delimited by a new line (0x0A)
     @Override
     public byte[] getMessageDelimiter()
     {
@@ -115,20 +130,25 @@ public final class InputListener implements SerialPortMessageListener
                 };
     }
 
+    //  indeed it does
     @Override
     public boolean delimiterIndicatesEndOfMessage()
     {
         return true;
     }
 
+    //  this function is automatically triggered every time something is sent/received over serial
     @Override
     public void serialEvent(SerialPortEvent event)
     {
+        //  serial input needs to be converted from array of bytes to UTF-8 string
         String input_raw = new String(event.getReceivedData(), StandardCharsets.UTF_8);
         String input = "";
 
+        //  valid requests sent from arduino all end with a ":"
         if (input_raw.contains(":"))
         {
+            //  removing the ":"
             input = input_raw.substring(0, input_raw.indexOf(":"));
             System.out.println(input);
         }
@@ -137,20 +157,26 @@ public final class InputListener implements SerialPortMessageListener
             return;
         }
 
+        //  api request to get info about the current status of the player
+        //  contains info like what song is playing, whether shuffle is on, etc.
         String player_info_raw = getRequest("https://api.spotify.com/v1/me/player", http_client, response_handler);
         JSONObject player_info = null;
 
         if (player_info_raw != null)
         {
+            //  get request function returns the http status code and response as well as the json data
+            //  so we cut it down to only the json data
             player_info_raw = player_info_raw.substring(player_info_raw.indexOf("{") - 1);
             player_info = new JSONObject(player_info_raw);
         }
 
+        //  initializing some status variables
         boolean shuffle_state = false;
         String loop_state = "off";
         boolean playing_state = false;
         String song_data = "";
 
+        //  if data was received, we grab the data we need and put it into a corresponding variable
         if (player_info != null)
         {
             shuffle_state = player_info.getBoolean("shuffle_state");
@@ -159,14 +185,17 @@ public final class InputListener implements SerialPortMessageListener
             song_data = player_info.getJSONObject("item").getJSONArray("artists").getJSONObject(0).getString("name") + " - " + player_info.getJSONObject("item").getString("name") + "\n";
         }
 
+        //  now looking at serial input from arduino
         switch (input)
         {
             case "Shuffle":
+                //  api request to set shuffle to the opposite of what it currently is
                 System.out.println(putRequest("https://api.spotify.com/v1/me/player/shuffle?state=" + !shuffle_state, http_client, response_handler));
                 break;
             case "Loop":
                 String new_loop_state;
 
+                //  go to next loop state (off -> track -> context ->)
                 switch (loop_state)
                 {
                     case "off":
@@ -180,14 +209,19 @@ public final class InputListener implements SerialPortMessageListener
                         break;
                 }
 
+                //  api request to send the new loop state
                 System.out.println(putRequest("https://api.spotify.com/v1/me/player/repeat?state=" + new_loop_state, http_client, response_handler));
                 break;
             case "Prev":
+                //  api request to go to prev track
                 System.out.println(postRequest("https://api.spotify.com/v1/me/player/previous", http_client, response_handler));
                 break;
             case "Play":
                 String uri;
 
+                //  different uri to either play or pause song
+                //  if song is playing, we want to pause it
+                //  if song is paused, we want to play it
                 if (playing_state)
                 {
                     uri = "https://api.spotify.com/v1/me/player/pause";
@@ -197,39 +231,51 @@ public final class InputListener implements SerialPortMessageListener
                     uri = "https://api.spotify.com/v1/me/player/play";
                 }
 
+                //  send api request
                 System.out.println(putRequest(uri, http_client, response_handler));
                 break;
             case "Next":
+                //  sent api request to go to next track
                 System.out.println(postRequest("https://api.spotify.com/v1/me/player/next", http_client, response_handler));
                 break;
             case "Volume":
+                //  volume input will look something like "Volume:58" so we need to trim everything except the number itself
                 String volume = input_raw.substring(input_raw.indexOf(":") + 1, input_raw.indexOf("\n") - 1);
                 System.out.println("Volume is: " + volume);
+                //  api request to change volume
                 System.out.println(putRequest("https://api.spotify.com/v1/me/player/volume?volume_percent=" + volume, http_client, response_handler));
                 break;
             default:
+                //  sometimes when loading the arduino it sends weird inputs
                 System.out.println("Invalid input.");
                 break;
         }
     }
 
     static public void main(String[] args) throws InterruptedException {
+        //  setting up serial port
+        //  arduino is on COM3
         port = SerialPort.getCommPort("COM3");
         port.openPort();
+        //  arduino is set to 9600
         port.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
         port.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
 
         InputListener listener = new InputListener();
         port.addDataListener(listener);
 
+        //  need to wait 2 seconds-ish to make sure the arduino is ready to receive data
+        //  not sure why this works
         Thread.sleep(2000);
 
         System.out.println("Started");
 
         String last_song = "";
 
+        //  infinite loop to send what song is currently playing
         while (true)
         {
+            //  loops every 1 second
             try
             {
                 Thread.sleep(1000);
@@ -239,9 +285,11 @@ public final class InputListener implements SerialPortMessageListener
                 e.printStackTrace();
             }
 
+            //  get current player info
             String player_info_raw = getRequest("https://api.spotify.com/v1/me/player", http_client, response_handler);
             JSONObject player_info = null;
 
+            //  trim everything except json data
             if (player_info_raw != null)
             {
                 player_info_raw = player_info_raw.substring(player_info_raw.indexOf("{") - 1);
@@ -250,13 +298,16 @@ public final class InputListener implements SerialPortMessageListener
 
             String song_data = "";
 
+            //  get the current song and artist
             if (player_info != null)
             {
                 song_data = player_info.getJSONObject("item").getJSONArray("artists").getJSONObject(0).getString("name") + " - " + player_info.getJSONObject("item").getString("name") + "\n";
             }
 
+            //  if the song has changed
             if (!song_data.equals(last_song))
             {
+                //  write to serial
                 last_song = song_data;
                 port.writeBytes(song_data.getBytes(), song_data.length());
             }
